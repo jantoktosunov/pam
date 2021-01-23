15725c04f9554fe48e001bfd46a330d573ae285e
jantoktosunov <zhanysh.toktosunov@gmail.com>
2021-01-23 14:20:40 +0100

    Add data.sql with initial data for Customer

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO CUSTOMER(firstName, lastName, address, EMAIL) VALUES
+('John', 'Doe', 'Janallee 10', 'jdoe@mail.com'),
+('Max', 'Mustermann', 'Zwickauer Straße 2', 'max90@gmail.com'),
+('John', 'Smith', 'Astrasse 11', 'jsmith@hotmail.com');
+
+
