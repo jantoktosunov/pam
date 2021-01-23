ab529ac84ac37276dbc2790758be6dbae6c01386
jantoktosunov <zhanysh.toktosunov@gmail.com>
2021-01-23 14:06:38 +0100

    Add Customer model

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,37 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "CUSTOMER")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "FIRSTNAME")
+    private String firstName;
+
+    @Column(name = "LASTNAME")
+    private String lastName;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    public Customer(String firstName, String lastName, String address, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+    }
+}
