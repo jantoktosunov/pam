8de11bc7ad55acc92c2ea2fa2718ae0f01b9e4ba
jantoktosunov <zhanysh.toktosunov@gmail.com>
2021-01-24 21:42:49 +0100

    Add getCustomer() and createCustomer() methods to CustomerController

--- src/main/java/com/zhanysh/pam/controller/CustomerController.java
+++ src/main/java/com/zhanysh/pam/controller/CustomerController.java
@@ -3,15 +3,26 @@ package com.zhanysh.pam.controller;
 import com.sun.istack.NotNull;
 import com.zhanysh.pam.model.Customer;
 import com.zhanysh.pam.service.CustomerService;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.Optional;
 
+/**
+ * Rest Controller for <code>Customer</code>.
+ */
 @RestController
-@RequestMapping(value = "/api/customer")
+@RequestMapping(value = "/api/customers")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -21,18 +32,52 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+    /**
+     * Adds new Customer to database.
+     * @param customer - that will be added.
+     * @param contentType - type of header.
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<String> createCustomer(@Valid @RequestBody Customer customer,
+                                               @RequestHeader("Content-Type") String contentType) {
+        HttpHeaders headers = new HttpHeaders();
+        String format = "";
+        headers.set("Content-Type", "application/json");
+        if("application/xml".equals(contentType)) {
+            //headers.set("Content-Type", "application/xml");
+            format = "xml";
+        } else {
+           //headers.set("Content-Type", "application/json");
+            format = "json";
+        }
         customerService.createCustomer(customer);
-        return ResponseEntity.ok(customerService.findCustomerById(customer.getId()));
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}").query("format=" + format)
+                .buildAndExpand(customer.getId()).toUri();
+        return ResponseEntity.created(location)
+                .headers(headers)
+                .body(location.getPath() + "?" +location.getQuery());
     }
 
-    @GetMapping(value = "/json/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Customer> getCustomerJson(@PathVariable @NotNull Long id) {
-        return ResponseEntity.ok(customerService.findCustomerById(id));
-    }
-    @GetMapping(value = "/xml/{id}", produces = {MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<Customer> getCustomerXml(@PathVariable @NotNull Long id) {
-        return ResponseEntity.ok(customerService.findCustomerById(id));
+    /**
+     * Get existing customer by id.
+     * @param id - of requested customer.
+     * @param format - of response. Should be either xml or json.
+     * @return
+     */
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> getCustomer(@PathVariable Long id, @RequestParam String format) {
+        HttpHeaders headers = new HttpHeaders();
+        if(format.equals("xml")) {
+            headers.set("Content-Type", "application/xml");
+        }
+        if(format.equals("json")) {
+            headers.set("Content-Type", "application/json");
+        }
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(customerService.findCustomerById(id));
     }
 }
